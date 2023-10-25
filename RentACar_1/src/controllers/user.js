4b51d65975199ab14eb54759410a196a7212767b
Bunyamin Genc <112567741+Bunyamin54@users.noreply.github.com>
2023-10-25 23:45:01 +0200

    crud

--- RentACar_1/src/controllers/user.js
+++ RentACar_1/src/controllers/user.js
@@ -5,3 +5,57 @@
 // User Controller:
 
 const User = require('../models/user')
+
+ /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "List Users"
+            #swagger.description = `
+                You can send query with endpoint for search[], sort[], page and limit.
+                <ul> Examples:
+                    <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
+                    <li>URL/?<b>sort[field1]=1&sort[field2]=-1</b></li>
+                    <li>URL/?<b>page=2&limit=1</b></li>
+                </ul>
+            `
+        */
+
+// Listeleme 
+module.exports = {
+
+ list: async (req, res) => {
+
+
+
+
+    const data  = await res.getModelList(User)
+
+    res.status(200).send ({
+
+        error: false,
+        details:await res.getModelListDetails(user),
+        data
+    })
+ },
+   
+ // Crud 
+
+   creat:async   {
+
+ /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "Create User"
+            #swagger.parameters['body'] = {
+                in: 'body',
+                required: true,
+                schema: {
+                    "username": "test",
+                    "password": "1234",
+                    "email": "test@site.com",
+                    "isActive": true,
+                    "isAdmin": false,
+                }
+            }
+        */
+       const data = await User.create()
+   }
+}
