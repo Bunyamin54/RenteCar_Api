f3d74c45bb098a16c70345ea27b7e343dfe0cf99
Bunyamin Genc <112567741+Bunyamin54@users.noreply.github.com>
2023-10-26 13:42:22 +0200

    swagger added in logout and login

--- RentACar_1/src/controllers/auth.js
+++ RentACar_1/src/controllers/auth.js
@@ -9,10 +9,29 @@ const Token = require('../models/token')
 module.exports = {
 
     login: async (req, res)  =>  {
+         /*
+            #swagger.tags = ["Authentication"]
+            #swagger.summary = "Login"
+            #swagger.description = 'Login with username (or email) and password.'
+            #swagger.parameters["body"] = {
+                in: "body",
+                required: true,
+                schema: {
+                    "username": "test",
+                    "password": "1234",
+                }
+            }
+        */
 
     },
 
     logout:async (req, res) => {
+
+        /*
+            #swagger.tags = ["Authentication"]
+            #swagger.summary = "Logout"
+            #swagger.description = 'Delete token key.'
+        */
 
     },
 }
