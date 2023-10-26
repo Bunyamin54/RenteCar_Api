a1e1414247a4036ff13bd5d8d24934d97e019be8
Bunyamin Genc <112567741+Bunyamin54@users.noreply.github.com>
2023-10-26 13:41:26 +0200

    auth conroller

--- RentACar_1/src/controllers/auth.js
+++ RentACar_1/src/controllers/auth.js
@@ -3,3 +3,16 @@
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
 // Auth Controller:
+const User = require('../models/user')
+
+const Token = require('../models/token')
+module.exports = {
+
+    login: async (req, res)  =>  {
+
+    },
+
+    logout:async (req, res) => {
+
+    },
+}
